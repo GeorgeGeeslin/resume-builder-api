89cd553c6be5ed5efbdc4be5d9bb45f31196f8d6
George Geeslin <george.geeslin@gmail.com>
2020-12-25 00:03:26 -0600

    initial commit

---
+++ api/update.js
@@ -0,0 +1,30 @@
+import handler from "../libs/handler-lib";
+import dynamoDb from "../libs/dynamodb-lib";
+
+export const main = handler(async (event, context) => {
+    const data = JSON.parse(event.body);
+    const params = {
+        TableName: process.env.tableName,
+        // 'Key' defines the partition key and sort key of the item to be updated
+        Key: {
+            userId: "123",
+            resumeId: event.pathParameters.id
+        },
+        // 'UpdateExpression' defines the attributes to be updated
+        // 'ExpressionAttributeValues' defines the value in the update expression
+        UpdateExpression: "SET content = :content, thumbnail = :thumbnail, pdf = :pdf",
+        ExpressionAttributeValues: {
+            ":content": data.content || null,
+            ":thumbnail": data.thumbnail || null,
+            ":pdf": data.pdf || null
+        },
+        // 'ReturnValues' specifies if and how to return the item's attributes,
+        // where ALL_NEW returns all attributes of the item after the update; you
+        // can inspect 'result' below to see how it works with different settings
+        ReturnValues: "ALL_NEW",
+    };
+
+    await dynamoDb.update(params);
+
+    return { status: true };
+});
