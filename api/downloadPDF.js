af84b141c9375391abd0ab922ee76b00cf2f165b
George Geeslin <george.geeslin@gmail.com>
2021-01-17 20:46:41 -0600

    using chrome-aws-lambda

---
+++ api/downloadPDF.js
@@ -0,0 +1,39 @@
+import chromium from "chrome-aws-lambda";
+import handler from "../libs/handler-lib";
+
+export const main = handler(async (event, context) => {
+
+    // get POST body.
+    const html = JSON.parse(event.body);
+
+        //Create browser object
+        const browser = await chromium.puppeteer.launch({
+            headless: true,
+            executablePath: await chromium.executablePath,
+            args: chromium.args
+        });
+
+        //Create page object.
+        const page = await browser.newPage();
+
+        //Build page from html string.
+        await page.setContent(html.data);
+
+        // Create pdf file with puppeteer
+        const pdf = await page.pdf(
+            {
+                format: 'Letter',
+                margin: {
+                    top: '0.5in',
+                    bottom: '0.5in',
+                    left: '0.5in',
+                    right: '0.5in'
+                },
+                pageRanges: '1-2'
+            }
+        );
+
+        const resume = pdf.toString('base64');
+
+        return resume;
+});
